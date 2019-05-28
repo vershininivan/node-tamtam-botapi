e9b56473ff9737f39132a3ae441c94ff958e5f9d
Ivan Vershinin <ivan.vershinin@corp.mail.ru>
2019-05-28 18:13:32 +0300

    fix

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-module.exports = require('./core/tamtam');
+//module.exports = require('./core/tamtam');
+
+const TamTamBot = require('./core/tamtam');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const TOKEN = 'wzkaE8SrN9Nmu9I-CdSfGjnwxAdKO2CYVv8PfTGvodc';
+
+const bot = new TamTamBot(TOKEN);
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/webhook`, (req, res) => {
+    bot.updateHandler(req.body);
+    res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(PORT, () => {
+    console.log(`Express server is listening on ${PORT}`);
+});
+
+let body = {};
+body.text = 'test message';
+
+bot.sendMessage(undefined, 52264184421, body);
+
+bot.getAllChats();
+
+bot.on('message_created', msg => {
+    bot.sendMessage(undefined, msg.recipient.chat_id, body);
+});
